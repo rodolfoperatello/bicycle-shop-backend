3ee005c1f034bf341a23d39da349e7f0d4e6b804
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-09-17 14:12:46 -0300

    Updating ProductRepository and ProductService methods

--- src/main/java/br/com/exactalabs/bicycleshop/repository/ProductRepository.java
+++ src/main/java/br/com/exactalabs/bicycleshop/repository/ProductRepository.java
@@ -1,4 +1,32 @@
 package br.com.exactalabs.bicycleshop.repository;
 
-public interface ProductRepository {
+import br.com.exactalabs.bicycleshop.entity.Product;
+import org.hibernate.annotations.NamedNativeQuery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Collection;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query(value = "SELECT t from Product t order by price")
+    Collection<Product> findProductByPriceAsc();
+
+    @Query(value = "SELECT t from Product t order by price desc")
+    Collection<Product> findProductByPriceDesc();
+
+    @Query(value = "SELECT t from Product t where name like %?1% order by name ASC")
+    Collection<Product> findProductByName(String productName);
+
+
+//    @Query(value = "SELECT t from Product t left join productCategory p on t.productCategory = p.id where p.name like %?1%")
+//    Collection<Product> findProductByCategory(String categoryName);
+
+
+
+
+
+
+
+
 }
--- src/main/java/br/com/exactalabs/bicycleshop/service/ProductService.java
+++ src/main/java/br/com/exactalabs/bicycleshop/service/ProductService.java
@@ -1,4 +1,49 @@
 package br.com.exactalabs.bicycleshop.service;
 
+
+import br.com.exactalabs.bicycleshop.entity.Product;
+import br.com.exactalabs.bicycleshop.repository.ProductCategoryRepository;
+import br.com.exactalabs.bicycleshop.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
 public class ProductService {
+
+    private ProductRepository productRepository;
+    private ProductCategoryRepository productCategoryRepository;
+
+    public ProductService(ProductRepository productRepository, ProductCategoryRepository productCategoryRepository){
+        this.productRepository = productRepository;
+        this.productCategoryRepository = productCategoryRepository;
+    }
+
+    public Product saveProduct(Product product) {
+        this.productCategoryRepository.save(product.getProductCategory());
+        return this.productRepository.save(product);
+    }
+
+    public void deleteProductById(Long id) {
+        this.productRepository.deleteById(id);
+    }
+
+    public Collection<Product> findProductByPriceAsc() {
+        return this.productRepository.findProductByPriceAsc();
+    }
+
+    public Collection<Product> findProductByPriceDesc() {
+        return this.productRepository.findProductByPriceDesc();
+    }
+
+    public Collection<Product> findProductByName(String name){
+        return this.productRepository.findProductByName(name);
+    }
+
+    public Collection<Product> findProductByCategory(String name){
+        return this.productRepository.findProductByCategory(name);
+    }
+
+
+
 }
