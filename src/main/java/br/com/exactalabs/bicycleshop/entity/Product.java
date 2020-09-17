b6fa2a061892d403f6d2c28c4acbc99d027f07e3
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-09-17 11:31:08 -0300

    Creating ORM, get and set Product class

--- src/main/java/br/com/exactalabs/bicycleshop/entity/Product.java
+++ src/main/java/br/com/exactalabs/bicycleshop/entity/Product.java
@@ -1,18 +1,29 @@
 package br.com.exactalabs.bicycleshop.entity;
 
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "product")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull(message = "O nome não pode ser nulo")
+    @NotBlank(message = "O nome não pode ser vazio")
     private String name;
+    @NotNull(message = "A categoria não pode ser nulo")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "productCategory")
     private ProductCategory productCategory;
+    @NotNull(message = "O preço não pode ser nulo")
     private BigDecimal price;
 
-    public Product() {
-
-    }
-
-    public Product (String name, ProductCategory productCategory, BigDecimal price) {
+    public Product(String name, ProductCategory productCategory, BigDecimal price) {
         this.name = name;
         this.productCategory = productCategory;
         this.price = price;
@@ -22,23 +33,33 @@ public class Product {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public ProductCategory getProductCategory() {
         return productCategory;
+    }
+
+    public void setProductCategory(ProductCategory productCategory) {
+        this.productCategory = productCategory;
     }
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public void setPrice(BigDecimal price) {
         this.price = price;
     }
 
-    public void setProductCategory(ProductCategory productCategory) {
-        this.productCategory = productCategory;
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", productCategory=" + productCategory +
+                ", price=" + price +
+                '}';
     }
 }
